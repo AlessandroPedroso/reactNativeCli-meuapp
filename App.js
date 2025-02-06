57a3791caa2085a5b5afbcfabd1020e7dc5e3902
Your Name <alessanrospedroso@outlook.com>
2025-02-06 12:15:25 -0300

    primiero commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+      <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>
+        Meu primeiro App
+      </Text>
+      <Text style={{fontSize: 20, colo: 'green'}}>Alessandro</Text>
+    </View>
+  );
+}
+
+export default App;
