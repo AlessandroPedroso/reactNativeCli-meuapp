fbee4bba73d6d0e2fc2865a0e7f90e3bb7f50f12
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2025-02-21 09:01:36 -0300

    aplicando grupos de estilos

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import {View, Text, Image} from 'react-native';
+import {useState} from 'react'
+import { View, Text, Image, Button } from 'react-native';
 
 function App() {
-  let nome = 'Alessandro Pedroso';
-  let img = 'https://sujeitoprogramador.com/reactlogo.png';
+  // let nome = 'Alessandro Pedroso';
+  // let img = 'https://sujeitoprogramador.com/reactlogo.png';
+
+  const [nome, setNome] = useState('Alessandro')
+  const [idade, setIdade] = useState(20);
+  
+  function entrar(nome,idade) {
+    setNome(nome)
+    setIdade(idade)
+  }
 
   return (
-    <View>
-      <Text>Hello World!</Text>
-      <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>
+    <View style={{marginTop:25}}>
+
+      <Button title='Mudar nome' onPress={() => entrar("Alessandro Schuquel Pedroso",33)} />
+      <Text style={{ fontSize: 19 }}>{nome}</Text>
+      <Text style={{ fontSize: 17 }}>{idade }</Text>
+      {/* <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>
         Meu primeiro App
-      </Text>
-      <Text style={{fontSize: 20, colo: 'green'}}>Alessandro</Text>
+      </Text> */}
+      {/* <Text style={{fontSize: 20, color: 'green'}}>Alessandro</Text>
 
       <Image style={{width: 300, height: 300}} source={{uri: img}} />
 
-      <Text style={{fontSize: 20}}>{nome}</Text>
+      <Text style={{fontSize: 20}}>{nome}</Text> */}
     </View>
   );
 }
